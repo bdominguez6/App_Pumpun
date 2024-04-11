3ee344dbad310bccc0a7f1d9b6a4bc0246615979
bdominguez6 <bdominguez@pumpunhost.com>
2024-04-11 18:28:04 +0200

    test(swipeScreen): testing swipe animation on search class

--- app/lib/widgets/search_screen/search_screen.dart
+++ app/lib/widgets/search_screen/search_screen.dart
@@ -1,10 +1,50 @@
+import 'package:app/data/dummy/dummy_brais.dart';
+import 'package:app/widgets/swipe_screen/interactive_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_card_swiper/flutter_card_swiper.dart';
 
 class SearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Search Screen'),
+    DummyBrais dummy = DummyBrais();
+    List<Container> cards = [
+      /// Image container
+      Container(
+        child: InteractiveImage(
+          book: dummy.book1,
+        ),
+      ),
+
+      /// Image container
+      Container(
+        child: InteractiveImage(
+          book: dummy.book2,
+        ),
+      ),
+
+      /// Image container
+      Container(
+        child: InteractiveImage(
+          book: dummy.book3,
+        ),
+      ),
+
+      /// Image container
+      Container(
+        child: InteractiveImage(
+          book: dummy.book4,
+        ),
+      ),
+    ];
+
+    return Scaffold(
+      body: Expanded(
+        child: CardSwiper(
+          cardsCount: cards.length,
+          cardBuilder: (context, index, percentThresholdX, percentThresholdY) =>
+              cards[index],
+        ),
+      ),
     );
   }
 }
--- app/lib/widgets/swipe_screen/swipe_screen.dart
+++ app/lib/widgets/swipe_screen/swipe_screen.dart
@@ -11,9 +11,6 @@ class SwipeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // double screenWidth = MediaQuery.of(context).size.width;
-    // double screenHeight = MediaQuery.of(context).size.height;
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent, // Fondo transparente
